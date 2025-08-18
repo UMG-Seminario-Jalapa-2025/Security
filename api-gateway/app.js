efecf386e134a53bbb5b143ad745500e58af00fa
Sincal200 <sincaljulio200@gmail.com>
2025-08-17 21:39:11 -0600

    Remove business partners service proxy and related configurations from app.js and docker-compose.yaml

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -75,21 +75,6 @@ const authMiddleware = async (req, res, next) => {
 };
 
 
-const serviceCRMURL = process.env.SERVICE_CRM_URL || "http://localhost:3002";
-const serviceGATEMASTERURL = process.env.SERVICE_GATEMASTER_URL || "http://localhost:3002";
-const businessPartnersUrl = process.env.BUSINESS_PARTNERS_URL || "http://business-partners-service:8082";
-// Proxy para business-partners-service (incluyendo Swagger) protegido con authMiddleware
-app.use(
-  "/api/business-partners",
-  authMiddleware,
-  createProxyMiddleware({
-    target: businessPartnersUrl,
-    changeOrigin: true,
-    pathRewrite: {
-      "^/api/business-partners": "",
-    },
-  })
-);
 
 // Set up proxy middleware for each service
 
@@ -108,41 +93,6 @@ app.use(
     },
   })
 );
-
-app.use(
-  "/api/crm",
-  authMiddleware,
-  createProxyMiddleware({
-    target: serviceCRMURL,
-    changeOrigin: true,
-    pathRewrite: {
-      "^/api/crm": "",
-    },
-    onProxyReq: (proxyReq, req, res) => {
-      if (req.method === "POST" && req.headers["content-type"]) {
-        proxyReq.setHeader("Content-Type", req.headers["content-type"]);
-      }
-    },
-  })
-);
-
-app.use(
-  "/api/gatemaster",
-  authMiddleware,
-  createProxyMiddleware({
-    target: serviceGATEMASTERURL,
-    changeOrigin: true,
-    pathRewrite: {
-      "^/api/gatemaster": "",
-    },
-    onProxyReq: (proxyReq, req, res) => {
-      if (req.method === "POST" && req.headers["content-type"]) {
-        proxyReq.setHeader("Content-Type", req.headers["content-type"]);
-      }
-    },
-  })
-);
-
 
 const port = process.env.PORT || 3000;
 
